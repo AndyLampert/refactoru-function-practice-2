92c095c804999038be0b999321859c0bdf71e7f8
AndyLampert <lamperta@gmail.com>
2014-04-17 19:11:55 -0600

    committing progress

--- main.js
+++ main.js
@@ -8,11 +8,43 @@ var totalLetters = function(arr){
 	return arr.join('').length;
 }
 // #3
+var keyValue = function(key, value){
+	 
+}
 
 // #4
- 
-// #5
+var negativeIndex = function(arr, negNum){
 
+} 
+// #5
+var removeM = function(str){
+	// works if there is only 1 m
+	// if(str.indexOf('m') > -1){
+	// 	var noMStr = str.replace('m','');
+	// 	return noMStr;
+	// } else {
+	// 	return str;
+	// }
+	
+
+	// attempted solution for multiple m's
+	var newArr = str.split('');
+	for (var i=0; i<newArr.length; i++){
+		if (newArr[i] === 'm') {
+			newArr.splice[i];
+		}
+	}
+	// change string to array
+	// for loop
+	// if array value === 'm', remove it
+	// end loop
+	// put back into a string
+	// return new string
+	
+
+
+
+}
 // #6
 
 // #7
